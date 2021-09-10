8872cac011f70380cead01eb96ecff59d254cc92
XoanTeira08 <82066695+XoanTeira08@users.noreply.github.com>
2021-09-10 14:01:51 -0600

    Update newtonR.m

--- newtonR.m
+++ newtonR.m
@@ -1,16 +1,22 @@
+% Una implementacion del metodo de regla Newton-Raphson para busqueda de raices en un 
+% invervalo  dado 
 function xs=newtonR(f,fdev,x,total)
-  fprintf('Metodo de Newton-Raphson\n\n');
+  fprintf('|Metodo de Newton-Raphson|\n\n');
   i=1;
   error=2;
+  %PRIMERA INTERACCION
   fvalor=feval(f,x);
   fdevvalor=feval(fdev,x);
   xs=x-(fvalor/fdevvalor);
   error=abs(xs-x);
+  %CAMBIANDO LAS VARIABLES
   x=xs;
   fvalor=feval(f,x);
   fdevvalor=feval(fdev,x);
+  %PRIMERA IMPRESION
   fprintf('Iter. \t xr \t\t ea \n');
   fprintf('%2i \t %f \t %f\n', i, xs, error);
+  %CICLO DE REPETICION
   while error>total
     xs=x-(fvalor/fdevvalor);
     error=abs(xs-x);
@@ -20,5 +26,6 @@ function xs=newtonR(f,fdev,x,total)
     i = i + 1;
     fprintf('%2i \t %f \t %f\n', i, xs, error);
   end %while
+  %IMPRESION DE RESULTADOS 
    fprintf('\n La mejor aproximacion a la raiz tomando una tolerancia de %f es x = %f \n y se realizaron %i iteraciones\n', total, xs, i);
 
